557a2001a25694c5b51259ec35e5c5cc3d60de4c
M Tayyab Saeed <tayyab.saeed05@gmail.com>
2017-11-26 01:05:20 +0100

    Routes added with directories and files for GUI
    This commit solely purpose is to make it clear to the contributors of
    this project that where would your GUI code do.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('courses/index', { title: 'Learning Hub' });
+  res.render('homepage/index', { title: 'Learning Hub' });
+});
+
+/* GET instructor profile course page. */
+router.get('/instructor/instructor-profile', function(req, res, next) {
+    res.render('instructor/instructor-profile', { title: 'Learning Hub' });
+});
+
+/* GET a make course page. */
+router.get('/instructor/make-course', function(req, res, next) {
+    res.render('instructor/make-course', { title: 'Learning Hub' });
+});
+
+/* GET student profile page. */
+router.get('/student/student-profile', function(req, res, next) {
+    res.render('student/student-profile', { title: 'Learning Hub' });
+});
+
+/* GET inside a course page that a student has registered. */
+router.get('/student/registered-course', function(req, res, next) {
+    res.render('student/registered-course', { title: 'Learning Hub' });
 });
 
 /* GET faqs page. */
