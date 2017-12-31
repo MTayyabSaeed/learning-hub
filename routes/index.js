b2a6e5129ffb5221fc5a07e6e99b13129932835c
W.J.B <waqaace.j.bhatti@student.fh-kiel.de>
2017-12-31 05:27:08 +0100

    Forum tabs and route created

--- routes/index.js
+++ routes/index.js
@@ -26,7 +26,7 @@ router.get('/instructor/instructor-profile', function(req, res, next) {
 
 
 
-router.get('/sockets', function(req, res, next) {
+router.get('/Forum', function(req, res, next) {
     res.render('sockets/sockets', { title: 'Learning Hub' });
 
 
