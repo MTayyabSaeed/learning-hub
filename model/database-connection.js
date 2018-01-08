dd578d06f285d4850611a8e412229e4b14f5e3fb
wjb786 <33326348+wjb786@users.noreply.github.com>
2018-01-08 02:27:51 +0100

    changed database name in sync with master

--- model/database-connection.js
+++ model/database-connection.js
@@ -3,7 +3,7 @@ var connection = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
     password : '',
-    database : 'learning hub'
+    database : 'learninghub'
 });
 connection.connect(function(err){
     if(!err) {
